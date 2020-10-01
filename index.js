dc3c47ee4a9f2d689776fff5b0cad6bd8db60909
Frank Tan <franktanah@gmail.com>
2020-10-01 21:56:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.post('/item/add', (req, res) => {
   newItem.save().then(item => res.redirect('/'));
 });
 
-const port = 3050;
+const port = 3000;
 
 app.listen(port, () => console.log('Server running...'));
